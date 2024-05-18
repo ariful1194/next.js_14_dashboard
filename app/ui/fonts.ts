b89dbc1ceff87112e274d2d23ed4eb3250e31043
Ariful Islam <arifulislam1194@gmail.com>
2024-05-18 11:12:09 +0600

    add fonts

--- app/ui/fonts.ts
+++ app/ui/fonts.ts
@@ -2,7 +2,7 @@ import { Inter, Lato, Lusitana } from 'next/font/google';
 
 export const inter = Inter({ subsets: ['latin'] });
 export const lato = Lato({ subsets: ['latin'], weight: ['400', '900', '700'] });
-export const lalusitanato = Lusitana({
+export const lusitana = Lusitana({
   subsets: ['latin'],
   weight: ['400', '700'],
 });
